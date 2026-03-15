bcad9d1dcefe0f1ae43a7f3807d7cb892b1a303b
BoraAbramov <borya812@gmail.com>
2026-03-15 19:45:55 +0200

    update 5

--- problem solve.py
+++ problem solve.py
@@ -60,3 +60,76 @@ import pprint
 
 list3 = [4, 2, 1, 2, 3, -1, 3, 2, 2]
 pprint.pprint(get_count(list3))
+
+'''
+4
+{"apple":5, "banana":6, "kiwi":4} -> {5:"apple", 6:"banana", 4:"kiwi"}
+'''
+def reverse_dict(dict1: dict) -> dict:
+    '''
+
+    :param dict1: {<word>: <number> ... }
+    :return: dict { <number>: <word> }
+    '''
+    reverse_dict = dict()
+    for key, value in dict1.items():
+        reverse_dict[value] = key
+    return reverse_dict
+
+dict4 = {"apple":5, "banana":6, "kiwi":4}
+print(reverse_dict(dict4))
+
+
+'''
+5
+[1,2,3,4,5,6]
+-> {"even":3, "odd":3}
+'''
+def count_even_odd(nums: list) -> dict:
+    '''
+    :param nums: list of numbers
+    :return: dict {"even": count_even, "odd": count_odd}
+    '''
+    num_dict = {"even": 0, "odd": 0}
+    for _ in list5:
+        if _ % 2 == 0:
+            num_dict["even"] += 1
+        else:
+            num_dict["odd"] += 1
+    return num_dict
+
+
+list5 = [1,2,3,4,5,6]
+print(count_even_odd(list5))
+
+
+'''
+6
+cities = {
+    "Tokyo": {"language": "Japanese", "population": 37_400_000, "size": 2194, "country": "Japan"},
+    "Paris": {"language": "French", "population": 2_140_000, "size": 105, "country": "France"},
+    "New York": {"language": "English", "population": 8_419_000, "size": 783, "country": "USA"},
+    "London": {"language": "English", "population": 8_982_000, "size": 1572, "country": "UK"},
+    "Madrid": {"language": "Spanish", "population": 3_223_000, "size": 604, "country": "Spain"},
+    "Rome": {"language": "Italian", "population": 2_873_000, "size": 1285, "country": "Italy"}
+}
+
+-> ["Paris", "Rome", "Madrid", "New York", "London", "Tokyo"]
+(sorted by population from small to big)
+'''
+
+def get_cities_sorted_by_population(cities: dict) -> list:
+    '''
+
+    :param cities: {
+        <city_name>: {
+            "language": <language>,
+            "population": <population>,
+            "size": <city_area_km2>,
+            "country": <country>
+        }
+    }
+
+    :return: list of city names sorted by population (small → big)
+    '''
+    pass
